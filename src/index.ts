ae0b1e916b85da94305c8340eec937a6db7e6c91
yukinissie <bigtree2490@outlook.jp>
2023-06-25 17:59:56 +0900

    :sparkles: feat: add read status

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,51 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { Book, Query, Status } from './generated/graphql';
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+  enum Status {
+    UNREAD
+    READING
+    READED
+  }
 
-  # This "Book" type defines the queryable fields for every book in our data source.
   type Book {
     title: String
     author: String
+    status: Status
   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
+    books: [Book!]!
   }
 `;
 
-const books = [
+const books: Book[] = [
   {
     title: 'The Awakening',
     author: 'Kate Chopin',
+    status: Status.Reading,
   },
   {
     title: 'City of Glass',
     author: 'Paul Auster',
+    status: Status.Unread,
   },
 ];
 
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
-const resolvers = {
+const bookResolver: () => Book[] = () => books;
+
+const resolvers: { Query: Query } = {
   Query: {
-    books: () => books,
+    books: bookResolver(),
   },
 };
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
 });
